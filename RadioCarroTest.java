4fcf109e9ef59f778ffc2348e4df508a5f002a79
AlejandroR <rus25512@uvg.edu.gt>
2026-01-16 20:24:24 -0600

    Junit test

--- RadioCarroTest.java
+++ RadioCarroTest.java
@@ -12,5 +12,27 @@ public class RadioCarroTest {
 
         assertEquals(540, radio.getEstacionActual());
     }
+@Test
+    public void testCambiarBandaAFM() {
+        RadioCarro radio = new RadioCarro();
+        radio.prenderRadio();
+        radio.cambiarFM();   
+        assertEquals(Banda.FM, radio.getBandaActual());
+        assertEquals(87.9, radio.getEstacionActual());
+    }
 
+    @Test
+    public void testGuardarYCargarEstacion() {
+        RadioCarro radio = new RadioCarro();
+        radio.prenderRadio();
+        radio.cambiarFM();   
+
+        radio.avanzarEstacion(); // 88.1
+        radio.guardarEstacion(1);
+
+        radio.avanzarEstacion(); // 88.3
+        radio.cargarEstacion(1);
+
+        assertEquals(88.1, radio.getEstacionActual());
+    }
 }
